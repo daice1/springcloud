fbf51097e9522373cd0a7da5cb89f1511bf0a251
daice <daice@DESKTOP-0O8J0UF>
2017-11-27 14:22:15 +0800

    添加测试1类

---
+++ sample-git/src/main/java/com/dai/sample/bean/Test1Bean.java
@@ -0,0 +1,41 @@
+package com.dai.sample.bean;
+
+public class Test1Bean {
+
+	public String name;
+	
+	public String age;
+	
+	public String sex;
+
+	@Override
+	public String toString() {
+		return "Test1Bean [name=" + name + ", age=" + age + ", sex=" + sex + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
+}
