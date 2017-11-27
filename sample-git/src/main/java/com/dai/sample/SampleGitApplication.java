d684a12cc060be2129e6b189e9fcb11416b56a1e
daice <daice@DESKTOP-0O8J0UF>
2017-11-27 14:08:44 +0800

    初始导入

---
+++ sample-git/src/main/java/com/dai/sample/SampleGitApplication.java
@@ -0,0 +1,12 @@
+package com.dai.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGitApplication.class, args);
+	}
+}
